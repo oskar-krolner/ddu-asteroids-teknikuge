bbbe4e4bee40c01892f804242547739d3915f0b5
oskar-krolner <oskar.krolner@live.com>
2025-09-17 12:39:32 +0200

    .

--- asteroid.gd
+++ asteroid.gd
@@ -8,6 +8,7 @@ enum asteroidsize{large, medium, small, tiny}
 var speed := 50
 
 @onready var sprite = $Sprite2D
+@onready var cshape = $CollisionShape2D
 
 
 func _ready():
@@ -17,15 +18,19 @@ func _ready():
 		asteroidsize.large:
 			speed = randf_range(50, 100)
 			sprite.texture = preload("res://asteroid big.png")
+			cshape.shape = preload("res://resources/astroid_cshape_large.tres")
 		asteroidsize.medium:
 			speed = randf_range(100, 150)
 			sprite.texture = preload("res://asteroid medium.png")
+			cshape.shape = preload("res://resources/asteroid_cshape_medium.tres")
 		asteroidsize.small:
 			speed = randf_range(100, 200)
 			sprite.texture = preload("res://asteroid small.png")
+			cshape.shape = preload("res://resources/asteroid_cshape_small.tres")
 		asteroidsize.tiny:
 			speed = randf_range(150, 200)
 			sprite.texture = preload("res://asteroid tiny.png")
+			cshape.shape = preload("res://resources/asteroid_cshape_tiny.tres")
 		
 
 
@@ -33,4 +38,10 @@ func _ready():
 func _physics_process(delta):
 	global_position += movement_vector.rotated(rotation) * speed * delta
 	
+	
+	var screen_size = get_viewport_rect().size
+	if global_position.y <0:
+		global_position.y = screen_size.y
+	elif global_position.y > screen_size.y:
+		global_position.y = 0
 	
